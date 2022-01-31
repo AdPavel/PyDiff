b88ae57550d1821d039720f91cd482df3e2ee176
PavelAd <adap2@mail.ru>
2022-01-31 16:33:58 +0400

    рекурсии

--- book_4_test.py
+++ book_4_test.py
@@ -20,15 +20,42 @@
 #
 # func()
 
-def sum(ls):
-    total = 0
-    for i in ls:
-        total += i
-    print(total)
-    return None
+# рекурсии
 
-sum([1,2,3])
+# def sum(ls):
+# #    total = 0
+#     if len(ls) == 1:
+#         return ls[0]
+#     else:
+#         return ls[0] + sum(ls[1:])
+#
+# def count_el(ls):
+#     if len(ls) == 1:
+#         return 1
+#     else:
+#         return 1 + count_el(ls[1:])
+#
+# def max_element(ls):
+#     if len(ls) == 2:
+#         return ls[0] if ls[0] > ls[1] else ls[1]
+#     sub_max = max_element(ls[1:])
+#     return ls[0] if ls[0] > sub_max else sub_max
+#
+#
+# ls = [1,2,3,5,6]
+# print(sum(ls))
+# print(count_el(ls))
+# print(max_element(ls))
 
+# быстрая сортировка
 
+def quick_sort(ls):
+    if len(ls) < 2:
+        return ls
+    else:
+        middle = ls[0]
+        less = [i for i in ls[1:] if i <= middle]
+        greater = [i for i in ls[1:] if i > middle]
+        return quick_sort(less) + [middle] + quick_sort(greater)
 
-
+print(quick_sort([12, 2, 3, 5, 87, 5, 4, 1]))
