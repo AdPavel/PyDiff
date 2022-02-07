ffbf92a39fb8308df1d380e31c7afa7c21b32e8b
PavelAd <adap2@mail.ru>
2022-02-07 17:58:34 +0400

    requests: urls from html

--- basics_and_application.py
+++ basics_and_application.py
@@ -1342,5 +1342,47 @@
 # else:
 #     print('No')
 
+#=====================================================================
+# Вашей программе на вход подается ссылка на HTML файл.
+# Вам необходимо скачать этот файл, затем найти в нем все ссылки вида <a ... href="..." ... > и вывести список
+# сайтов, на которые есть ссылка.
+#
+# Сайтом в данной задаче будем называть имя домена вместе с именами поддоменов. То есть, это последовательность
+# символов, которая следует сразу после символов протокола, если он есть, до символов порта или пути, если они есть,
+# за исключением случаев с относительными ссылками вида
+# <a href="../some_path/index.html">.
+#
+# Сайты следует выводить в алфавитном порядке.
+#
+# Пример HTML файла:
+#
+# <a href="http://stepic.org/courses">
+# <a href='https://stepic.org'>
+# <a href='http://neerc.ifmo.ru:1345'>
+# <a href="ftp://mail.ru/distib" >
+# <a href="ya.ru">
+# <a href="www.ya.ru">
+# <a href="../skip_relative_links">
+#
+# Пример ответа:
+#
+# mail.ru
+# neerc.ifmo.ru
+# stepic.org
+# www.ya.ru
+# ya.ru
+
+import requests, re
+
+ls_all = []
+match_set = set()
+any_link = re.compile(r'<a.*href=[\"\'](http://|ftp://|https://|\.\.)?(.*?)(\:.*|\/.*)?[\'\"].*>')
+res = requests.get(input())
+if res.status_code == 200:
+    ls_all = any_link.findall(res.text)
+    match_set = sorted({i[1] for i in ls_all})
+
+[print(link) for link in match_set]
+
 
 
