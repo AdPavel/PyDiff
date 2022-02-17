0ebf649470fc6112e3826cd800cbb045d70ddaad
PavelAd <adap2@mail.ru>
2022-02-17 17:35:21 +0400

    XML task

--- basics_and_application.py
+++ basics_and_application.py
@@ -1632,17 +1632,83 @@
 # tree.write('ba/example_mod.xml')
 
 #========== LXML =======================
-from lxml import etree
-import requests
+# from lxml import etree
+# import requests
+#
+# res = requests.get('https://docs.python.org/3/')
+# print(res.status_code)
+# print(res.headers['Content-Type'])
+#
+# par = etree.HTMLParser()
+# root = etree.fromstring(res.text, par)
+#
+# for element in root.iter('a'):
+#     print(element, element.attrib)
 
-res = requests.get('https://docs.python.org/3/')
-print(res.status_code)
-print(res.headers['Content-Type'])
+#==================================================================
+# Вам дано описание пирамиды из кубиков в формате XML.
+# Кубики могут быть трех цветов: красный (red), зеленый (green) и синий (blue﻿).
+# Для каждого кубика известны его цвет, и известны кубики, расположенные прямо под ним.
+# Пример:
+#
+# <cube color="blue">
+#   <cube color="red">
+#     <cube color="green">
+#     </cube>
+#   </cube>
+#   <cube color="red">
+#   </cube>
+# </cube>
+# Введем понятие ценности для кубиков. Самый верхний кубик, соответствующий корню XML документа имеет ценность 1.
+# Кубики, расположенные прямо под ним, имеют ценность 2. Кубики, расположенные прямо под нижележащими кубиками,
+# имеют ценность 3. И т. д.
+# Ценность цвета равна сумме ценностей всех кубиков этого цвета.
+# Выведите через пробел три числа: ценности красного, зеленого и синего цветов.
 
-par = etree.HTMLParser()
-root = etree.fromstring(res.text, par)
-
-for element in root.iter('a'):
-    print(element, element.attrib)
+# <cube color="blue"><cube color="red"><cube color="green"></cube></cube><cube color="red"></cube></cube>
+# 4 3 1
 
 
+from xml.etree import ElementTree #import XMLParser
+
+
+class MaxDepth:                     # The target object of the parser
+    # maxDepth = 0
+    depth = 0
+    dict_color = {'blue': 0, 'red': 0, 'green': 0}
+
+    def start(self, tag, attrib):   # Called for each opening tag.
+        self.depth += 1
+        # if self.depth > self.maxDepth:
+        #     self.maxDepth = self.depth
+        # self.list_color[self.depth - 1] += self.depth
+        self.dict_color[attrib['color']] += self.depth
+
+    def end(self, tag):             # Called for each closing tag.
+        self.depth -= 1
+
+    def data(self, data):
+        pass            # We do not need to do anything with data.
+
+    def close(self):    # Called when all data has been parsed.
+        return self.dict_color
+
+
+target = MaxDepth()
+parser = ElementTree.XMLParser(target=target)
+
+exampleXml = input()
+# exampleXml = """
+#  <a>
+#    <b>
+#    </b>
+#    <b>
+#      <c>
+#        <d>
+#        </d>
+#      </c>
+#    </b>
+# </a>"""
+parser.feed(exampleXml)
+color_dict = parser.close()
+print(color_dict['red'], color_dict['green'], color_dict['blue'])
