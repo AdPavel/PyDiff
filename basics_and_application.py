53e2c9b414a4dd5a42f258aafd35d66e2ae3a6c5
PavelAd <adap2@mail.ru>
2022-02-21 18:38:10 +0400

    same commit

--- basics_and_application.py
+++ basics_and_application.py
@@ -1667,48 +1667,66 @@
 
 # <cube color="blue"><cube color="red"><cube color="green"></cube></cube><cube color="red"></cube></cube>
 # 4 3 1
+#
+#
+# from xml.etree import ElementTree #import XMLParser
+#
+#
+# class MaxDepth:                     # The target object of the parser
+#     # maxDepth = 0
+#     depth = 0
+#     dict_color = {'blue': 0, 'red': 0, 'green': 0}
+#
+#     def start(self, tag, attrib):   # Called for each opening tag.
+#         self.depth += 1
+#         # if self.depth > self.maxDepth:
+#         #     self.maxDepth = self.depth
+#         # self.list_color[self.depth - 1] += self.depth
+#         self.dict_color[attrib['color']] += self.depth
+#
+#     def end(self, tag):             # Called for each closing tag.
+#         self.depth -= 1
+#
+#     def data(self, data):
+#         pass            # We do not need to do anything with data.
+#
+#     def close(self):    # Called when all data has been parsed.
+#         return self.dict_color
+#
+#
+# target = MaxDepth()
+# parser = ElementTree.XMLParser(target=target)
+#
+# exampleXml = input()
+# # exampleXml = """
+# #  <a>
+# #    <b>
+# #    </b>
+# #    <b>
+# #      <c>
+# #        <d>
+# #        </d>
+# #      </c>
+# #    </b>
+# # </a>"""
+# parser.feed(exampleXml)
+# color_dict = parser.close()
+# print(color_dict['red'], color_dict['green'], color_dict['blue'])
+#===================== API ============
 
+import requests
+from datetime import date, timedelta
 
-from xml.etree import ElementTree #import XMLParser
+today = date.today()
+yesterday = today - timedelta(days=1)
+country = "Russia"
+endpoint = f"https://api.covid19api.com/country/{country}/status/confirmed"
+params = {"from": str(yesterday), "to": str(today)}
 
+response = requests.get(endpoint, params=params).json()
+total_confirmed = 0
+for day in response:
+    cases = day.get("Cases", 0)
+    total_confirmed += cases
 
-class MaxDepth:                     # The target object of the parser
-    # maxDepth = 0
-    depth = 0
-    dict_color = {'blue': 0, 'red': 0, 'green': 0}
-
-    def start(self, tag, attrib):   # Called for each opening tag.
-        self.depth += 1
-        # if self.depth > self.maxDepth:
-        #     self.maxDepth = self.depth
-        # self.list_color[self.depth - 1] += self.depth
-        self.dict_color[attrib['color']] += self.depth
-
-    def end(self, tag):             # Called for each closing tag.
-        self.depth -= 1
-
-    def data(self, data):
-        pass            # We do not need to do anything with data.
-
-    def close(self):    # Called when all data has been parsed.
-        return self.dict_color
-
-
-target = MaxDepth()
-parser = ElementTree.XMLParser(target=target)
-
-exampleXml = input()
-# exampleXml = """
-#  <a>
-#    <b>
-#    </b>
-#    <b>
-#      <c>
-#        <d>
-#        </d>
-#      </c>
-#    </b>
-# </a>"""
-parser.feed(exampleXml)
-color_dict = parser.close()
-print(color_dict['red'], color_dict['green'], color_dict['blue'])
+print(f"Total Confirmed Covid-19 cases in {country}: {total_confirmed}")
